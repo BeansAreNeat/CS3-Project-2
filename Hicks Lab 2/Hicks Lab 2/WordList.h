0e7e9434d7a88a6eedd702a48408118d88da6381
Joe Hicks <103856984+BeansAreNeat@users.noreply.github.com>
2023-09-06 01:21:21 -0400

    Add files via upload

--- Hicks Lab 2/Hicks Lab 2/WordList.h
+++ Hicks Lab 2/Hicks Lab 2/WordList.h
@@ -1,34 +1,109 @@
-#pragma once
 #include <string>
+#include <iostream>
 using namespace std;
 
 class WordOccurrence
 {
 public:
-    WordOccurrence(const string& word = "", int num = 0);
-    bool matchWord(const string&); // returns true if word matches stored
-    void increment(); // increments number of occurrences
-    string getWord() const;
-    int getNum() const;
+    WordOccurrence(const string& word = "", int num = 0) {
+        word_ = word;
+        num_ = num;
+    }
+
+    bool matchWord(const string& word) {
+        return word_ == word;
+    }
+
+    void increment() {
+        num_++;
+    }
+
+    string getWord() const {
+        return word_;
+    }
+
+    int getNum() const {
+        return num_;
+    }
 
 private:
     string word_;
     int num_;
 };
 
-class WordList
-{
+class WordList {
 public:
-    // add copy constructor, destructor, overloaded assignment
-    WordList();
-    WordList(const WordList&);
-    ~WordList();
-    void addWord(const string&);
-    void printList();
-private:
-    // a dynamically allocated array of WordOccurrences
-    // may or may not be sorted
-    WordOccurrence* wordArray_;
+    WordList(int initialSize = 50) // Constructor with initial size
+        : size_(initialSize), wordArray_(new WordOccurrence[initialSize]) {
+    }
 
+    // Copy constructor
+    WordList(const WordList& other)
+        : size_(other.size_), wordArray_(new WordOccurrence[other.size_]) {
+        // Copy each Word from the other WordList
+        for (int i = 0; i < size_; ++i) {
+            wordArray_[i] = other.wordArray_[i];
+        }
+    }
+
+    ~WordList() {
+        delete[] wordArray_;
+    }
+
+    void addWord(const string& word) {
+        if (size_ >= maxSize_) {
+            // Resize the array if it's full
+            resizeArray();
+        }
+
+        bool wordExists = false;
+        for (int i = 0; i < size_; i++) {
+            if (wordArray_[i].matchWord(word)) {
+                wordArray_[i].increment();
+                wordExists = true;
+                break;
+            }
+        }
+
+        if (!wordExists) {
+            wordArray_[size_] = WordOccurrence(word, 1);
+            size_++;
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < size_ - 1; ++i) {
+            for (int j = 0; j < size_ - i - 1; ++j) {
+                if (wordArray_[j].getNum() < wordArray_[j + 1].getNum()) {
+                    swap(wordArray_[j], wordArray_[j + 1]);
+                }
+            }
+        }
+    }
+
+    void printList() {
+        bubbleSort();
+        for (int i = 0; i < size_; ++i) {
+            if (wordArray_[i].getNum() > 0) {
+                cout << wordArray_[i].getWord() << ": " << wordArray_[i].getNum() << endl;
+            }
+        }
+    }
+
+private:
+    WordOccurrence* wordArray_;
     int size_;
+    int maxSize_ = 50; // Maximum size of the array
+
+    // Helper function to resize the array
+    void resizeArray() {
+        int newSize = maxSize_ * 2;
+        WordOccurrence* newArray = new WordOccurrence[newSize];
+        for (int i = 0; i < size_; ++i) {
+            newArray[i] = wordArray_[i];
+        }
+        delete[] wordArray_;
+        wordArray_ = newArray;
+        maxSize_ = newSize;
+    }
 };
